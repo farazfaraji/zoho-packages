852b62ae6321bc21e006e4ce7a03d34c65cdba3d
Faraz <faraz.faraji@gmail.com>
2021-03-17 16:09:27 +0300

    - getReport function to fetch report from zoho people

--- people/index.js
+++ people/index.js
@@ -5,6 +5,13 @@ class People extends ZohoAuth {
         super(uniq_name,client_id, client_secret, refresh_token)
     }
 
+    /**
+     *
+     * @param formName
+     * @param sIndex start index
+     * @param limit maximum 200
+     * @returns {Promise<*>}
+     */
     async getForms(formName,sIndex=1,limit=200) {
         try {
             return await this.customRequest(`https://people.zoho.com/people/api/forms/${formName}/getRecords?sIndex=${sIndex}&limit=${limit}`, "POST");
@@ -16,6 +23,11 @@ class People extends ZohoAuth {
         }
     }
 
+    /**
+     *
+     * @param formName
+     * @returns {Promise<Array>}
+     */
     async getFormsAllData(formName) {
         try {
             let result = [];
@@ -31,6 +43,24 @@ class People extends ZohoAuth {
             return data;
         }catch (e) {
             console.log(e);
+        }
+    }
+
+    /**
+     *
+     * @param sdate MM/dd/YYYY
+     * @param edate MM/dd/YYYY
+     * @param employee_id optional
+     * @returns {Promise<*>}
+     */
+    async getReport(sdate,edate,employee_id=null){
+        try {
+            return await this.customRequest(`https://people.zoho.com/people/api/attendance/getUserReport`,"GET",{sdate,edate,employee_id});
+        }catch (e) {
+            if (e.response !== undefined)
+                console.error(e.response.data);
+            else
+                console.error(e.message);
         }
     }
 }
